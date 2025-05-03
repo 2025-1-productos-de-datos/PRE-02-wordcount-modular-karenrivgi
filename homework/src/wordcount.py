f9a642b83505c3862e93076580f4969efdf45b96
Karen Rivera Giraldo <kariverag@unal.edu.co>
2025-05-03 11:01:20 -0500

    Modularizacion de wordcount

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,32 +1,15 @@
-import os
-
-from ._internals.write_count_words import write_count_words
-
-
-def read_all_lines():
-    all_lines = []
-    input_file_list = os.listdir("data/input/")
-
-    for filename in input_file_list:
-        with open("data/input/" + filename) as f:
-            lines = f.readlines()
-            all_lines.extend(lines)
-
-    return all_lines
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_into_words import split_in_words
+from ._internals.write_word_counts import write_count_words
 
 
 def main():
-
-    input_file_list = os.listdir("data/input/")
-
-    # count the frequency of the words in the files in the input directory
-    counter = {}
-    for filename in input_file_list:
-        with open("data/input/" + filename) as f:
-            for l in f:
-                for w in l.split():
-                    w = w.lower().strip(",.!?")
-                    counter[w] = counter.get(w, 0) + 1
+    all_lines = read_all_lines()
+    all_lines = preprocess_lines(all_lines)
+    words = split_in_words(all_lines)
+    counter = count_words(words)
 
     write_count_words(counter)
 
