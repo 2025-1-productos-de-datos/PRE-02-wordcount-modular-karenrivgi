9ab690630a9e65eb5dd5f00f6208663f6e9ce9e1
Karen Rivera Giraldo <kariverag@unal.edu.co>
2025-05-03 09:48:50 -0500

    Agg test migracion y refactorizacion wordcount

---
+++ homework/src/wordcount.py
@@ -0,0 +1,46 @@
+import os
+
+
+def read_all_lines():
+    all_lines = []
+    input_file_list = os.listdir("data/input/")
+
+    for filename in input_file_list:
+        with open("data/input/" + filename) as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+
+    return all_lines
+
+
+def write_count_words(counter):
+
+    # create the directory output/ if it doesn't exist
+    if not os.path.exists("data/output"):
+        os.makedirs("data/output")
+
+    # save the results using tsv format
+    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
+        for key, value in counter.items():
+            # write the key and value to the file
+            f.write(f"{key}\t{value}\n")
+
+
+def main():
+
+    input_file_list = os.listdir("data/input/")
+
+    # count the frequency of the words in the files in the input directory
+    counter = {}
+    for filename in input_file_list:
+        with open("data/input/" + filename) as f:
+            for l in f:
+                for w in l.split():
+                    w = w.lower().strip(",.!?")
+                    counter[w] = counter.get(w, 0) + 1
+
+    write_count_words(counter)
+
+
+if __name__ == "__main__":
+    main()
---
+++ homework/tests/test_migracion.py
@@ -0,0 +1,23 @@
+import os
+
+from homework.src.wordcount import main
+
+
+def test_migracion():
+
+    main()
+
+    if not os.path.exists("data/output/results.tsv"):
+        raise Exception("El archivo results.tsv no existe")
+
+    results = {}
+    with open("data/output/results.tsv", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    for line in lines:
+        key, value = line.strip().split("\t")
+        results[key] = int(value)
+
+    assert (
+        results.get("computational", 0) == 3
+    ), "Conteo incorrecto para 'computational'"
+    assert results.get("analytics", 0) == 5, "Conteo incorrecto para 'analytics'"
